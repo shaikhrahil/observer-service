75a381b0aca46c8f3137f2f1551b7fdcd9d058c9
shaikhrahil <smohammadrahil@gmail.com>
2021-09-19 16:42:09 +0530

    :recycle: refactor(accounts): used RetrieveUpdateDestroyAPIView instead of separate apis

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,9 @@
-# from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-from accounts.views import GetUser, UpdateUser, getProfile, signup, logout
+from accounts.views import UserController, signup, logout
 from django.urls import path
 
 urlpatterns = [
@@ -12,7 +11,5 @@ urlpatterns = [
     path("auth/login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/logout/", logout, name="logout"),
-    path("user/", getProfile, name="get_current_user"),
-    path("user/<int:pk>/", GetUser.as_view(), name="get_user"),
-    path("user/<int:pk>/", UpdateUser.as_view(), name="update_user"),
+    path("user/", UserController.as_view(), name="get_update_user"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -28,20 +28,10 @@ def logout(request):
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def getProfile(request):
-    user = UserSerializer(request.user)
-    return Response(user.data)
-
-
-class GetUser(generics.RetrieveAPIView):
+class UserController(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-
-class UpdateUser(generics.UpdateAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    def get_object(self, queryset=None):
+        return self.request.user
